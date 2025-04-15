a180b7062a98694497e726e23b813c87957035f8
FernandoVaras147 <110378850+FernandoVaras147@users.noreply.github.com>
2025-04-15 18:55:09 -0500

    Create LaserScanVisualizer.cs

---
+++ LaserScanVisualizer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LaserScanVisualizer : MonoBehaviour
+{
+    public void SetSensorData(Transform origin, Vector3[] directions, float[] ranges, float minRange, float maxRange)
+    {
+        // Aquí puedes implementar la lógica de visualización si quieres
+        Debug.Log("Visualización de láser no implementada aún.");
+    }
+}
